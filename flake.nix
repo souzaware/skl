3df218fd2d955559ff591c121ea1d5b69437fb08
Andrei de Souza <71538656+youngnix@users.noreply.github.com>
2026-01-21 00:38:36 -0300

    update flake to allow installation in nixos

--- flake.nix
+++ flake.nix
@@ -1,19 +1,93 @@
 {
+    description = "A single-key launcher for Linux";
+
     inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
         utils.url = "github:numtide/flake-utils";
     };
-    outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+
+    outputs = inputs: 
         let
-            pkgs = nixpkgs.legacyPackages.${system};
+            inherit (inputs) self nixpkgs utils;
+            platforms = [
+                "x86_64-linux"
+                "aarch64-linux"
+            ];
+
+            version = "0.1.0";
+
+            mkSkl = pkgs:
+                let
+                    system = pkgs.stdenv.hostPlatform.system;
+                in
+                    pkgs.stdenv.mkDerivation {
+                        pname = "skl";
+                        inherit version;
+
+                        nativeBuildInputs = with pkgs; [
+                            odin
+                            sdl3
+                            sdl3-ttf
+                            mold
+                        ];
+
+                        buildInputs = with pkgs; [
+                            odin
+                            sdl3
+                            sdl3-ttf
+                            makeWrapper
+                        ];
+
+                        src = ./.;
+
+                        buildPhase = ''
+                        runHook preBuild
+
+                        odin build . -o:speed -linker:mold -microarch:native -out:skl
+
+                        runHook postBuild
+                    '';
+
+                        installPhase = ''
+                        runHook preInstall
+
+                        mkdir -p $out/bin
+                        cp skl $out/bin
+
+                        makeWrapper $out/bin/skl $out/bin/skl-wrapped \
+                        --prefix LD_LIBRARY_PATH : "${
+                            nixpkgs.lib.makeLibraryPath [
+                                pkgs.sdl3
+                                pkgs.sdl3-ttf
+                            ]
+                        }"
+
+                        runHook postInstall
+                    '';
+
+                        meta = {
+                            description = "A single-key launcher for Linux";
+                            homepage = "https://github.com/souzaware/skl";
+                            mainProgram = "skl-wrapped";
+                        };
+                    };
         in
-            {
-            devShell = pkgs.mkShell {
-                buildInputs = with pkgs; [
-                    odin
-                    sdl3
-                    sdl3-ttf
-                ];
-            };
-        }
-    );
+            utils.lib.eachDefaultSystem (system:
+                let 
+                    pkgs = nixpkgs.legacyPackages.${system};
+                in
+                    {
+                    packages = {
+                        skl = mkSkl pkgs;
+                        default = self.packages.${system}.skl;
+                    };
+
+                    devShell = pkgs.mkShell {
+                        buildInputs = with pkgs; [
+                            odin
+                            sdl3
+                            sdl3-ttf
+                        ];
+                    };
+                });
 }
